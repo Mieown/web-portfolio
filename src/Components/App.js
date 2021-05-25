eceba37f1d4327cd0c0d977038bffa882874213a
Mieown <malinolofsson@hyperisland.se>
2021-05-25 15:32:26 +0200

    test

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">       
-      <h1>Hejsan</h1>
+      <h1>Hejsan Svejsan</h1>
       <Circle />
       </header>
       <main>
