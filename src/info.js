feff3a6e4d468d74b160f4b4866c3f97da475626
Malin Olofsson <maline.olofsson@hotmail.com>
2025-06-09 09:36:21 +0200

    changes

--- src/info.js
+++ src/info.js
@@ -1,89 +1,88 @@
-import whoolichen from './images/whoolichen.jpeg';
-import studioone from './images/studioone.jpeg';
-import shoelace from './images/shoelace.jpeg';
-import rota from './images/rota.png';
-import storiesboxtre from './images/storiesboxtre.jpeg';
-import ricolastart from './images/ricola-start.png';
-import feelgoodinsurancestart from './images/feelgoodinsurance-start.png';
-import mygamefabric from './images/mygame-insidefabric-start.png';
-import braivestart from './images/braive-start-start.png';
-import api from './images/api.png';
-import toungetwister from './images/tounge-twistersucess.png';
-import firstaidtherapy from './images/firstaidtherapy-nervous-start.png';
-import coscam from './images/coscam.jpeg';
-import simonerocha from './images/simonerocha.png';
-import hmconscious from './images/hmconscious.jpeg';
-import hmhome from './images/hmforms.jpeg';
+import whoolichen from "./images/whoolichen.jpeg";
+import studioone from "./images/studioone.jpeg";
+import rota from "./images/rota.png";
+import storiesboxtre from "./images/storiesboxtre.jpeg";
+import ricolastart from "./images/ricola-start.png";
+import feelgoodinsurancestart from "./images/feelgoodinsurance-start.png";
+import mygamefabric from "./images/mygame-insidefabric-start.png";
+import braivestart from "./images/braive-start-start.png";
+import api from "./images/api.png";
+import toungetwister from "./images/tounge-twistersucess.png";
+import firstaidtherapy from "./images/firstaidtherapy-nervous-start.png";
+import coscam from "./images/coscam.jpeg";
+import simonerocha from "./images/simonerocha.png";
+import hmconscious from "./images/hmconscious.jpeg";
+import hmhome from "./images/hmforms.jpeg";
 
-import rotastart from './images/rota-start-start.png';
-import rotaform from './images/rota-form.png';
-import rotastatus from './images/rota-status.png';
-import rotagoal from './images/rota-goal.png';
+import rotastart from "./images/rota-start-start.png";
+import rotaform from "./images/rota-form.png";
+import rotastatus from "./images/rota-status.png";
+import rotagoal from "./images/rota-goal.png";
 
-import feelgoodinsurancesecond from './images/feelgoodinsurance-second.png';
-import feelgoodinsurancethird from './images/feelgoodinsurance-third.png';
-import feelgoodinsurancelast from './images/feelgoodinsurance-last.png';
+import feelgoodinsurancesecond from "./images/feelgoodinsurance-second.png";
+import feelgoodinsurancethird from "./images/feelgoodinsurance-third.png";
+import feelgoodinsurancelast from "./images/feelgoodinsurance-last.png";
 
-import braivestarts from './images/braive-start.png';
-import braivetools from './images/braive-tools.png';
-import braivestartmobile from './images/braive-start-mobile.png';
-import braiverecommendmobile from './images/braive-recommendedprograms-mobile.png';
+import braivestarts from "./images/braive-start.png";
+import braivetools from "./images/braive-tools.png";
+import braivestartmobile from "./images/braive-start-mobile.png";
+import braiverecommendmobile from "./images/braive-recommendedprograms-mobile.png";
 
-import mygamefabricstart from './images/mygame-starttwo.png';
-import mygamefabricinside from './images/mygame-insidefabric.png';
-import mygamefabrickeypad from './images/mygame-keypad.png';
-import mygamefabricsky from './images/mygame-sky.png';
+import mygamefabricstart from "./images/mygame-starttwo.png";
+import mygamefabricinside from "./images/mygame-insidefabric.png";
+import mygamefabrickeypad from "./images/mygame-keypad.png";
+import mygamefabricsky from "./images/mygame-sky.png";
 
-import ricolasecond from './images/ricola-mainclimber.png';
-import ricolathird from './images/ricola-popupherbs.png';
-import ricolalast from './images/ricola-maintruck.png';
+import ricolasecond from "./images/ricola-mainclimber.png";
+import ricolathird from "./images/ricola-popupherbs.png";
+import ricolalast from "./images/ricola-maintruck.png";
 
-import firstaidtherapystart from './images/firstaidtherapy-start.png';
-import firstaidtherapysecond from './images/firstaidtherapy-nervous.png';
-import firstaidtherapythird from './images/firstaidtherapy-sad.png';
-import firstaidtherapylastlast from './images/heartapp.png';
+import firstaidtherapystart from "./images/firstaidtherapy-start.png";
+import firstaidtherapysecond from "./images/firstaidtherapy-nervous.png";
+import firstaidtherapythird from "./images/firstaidtherapy-sad.png";
+import firstaidtherapylastlast from "./images/heartapp.png";
 
-import toungetwisterstart from './images/toungetwister-start.png';
-import toungetwisecond from './images/tounge-twistermain.png';
-import toungetwistlast from './images/tounge-twisterfail.png';
+import toungetwisterstart from "./images/toungetwister-start.png";
+import toungetwisecond from "./images/tounge-twistermain.png";
+import toungetwistlast from "./images/tounge-twisterfail.png";
 
-import studiofirst from './images/studiofirst.png';
-import studiosecond from './images/studiosecond.png';
-import studiothird from './images/studiothird.png';
+import studiofirst from "./images/studiofirst.png";
+import studiosecond from "./images/studiosecond.png";
+import studiothird from "./images/studiothird.png";
 
-import whoolichenfirst from './images/whoolichen-last.png';
-import whoolichensecond from './images/whoolichen-second.png';
-import whoolichenthird from './images/whoolichen-third.png';
-import whoolichenlast from './images/whoolichen-first.png';
+import whoolichenfirst from "./images/whoolichen-last.png";
+import whoolichensecond from "./images/whoolichen-second.png";
+import whoolichenthird from "./images/whoolichen-third.png";
+import whoolichenlast from "./images/whoolichen-first.png";
 
-import shoelacepink from './images/shoelace-pink.png';
-import shoelacepurple from './images/shoelace-purple.png';
-import shoelaceyellow from './images/shoelace-yellow.png';
+import shoelacepink from "./images/shoelace-pink.png";
+import shoelacepurple from "./images/shoelace-purple.png";
+import shoelaceyellow from "./images/shoelace-yellow.png";
 
-import coscamstart from './images/coscam.png';
+import coscamstart from "./images/coscam.png";
 
-import otherstoriesbox1 from './images/otherstoriesbox1.png';
-import otherstoriesbox2 from './images/otherstoriesbox2.png';
-import otherstoriesbox3 from './images/otherstoriesbox3.png';
-import otherstoriesbox4 from './images/otherstoriesbox4.png';
+import otherstoriesbox1 from "./images/otherstoriesbox1.png";
+import otherstoriesbox2 from "./images/otherstoriesbox2.png";
+import otherstoriesbox3 from "./images/otherstoriesbox3.png";
+import otherstoriesbox4 from "./images/otherstoriesbox4.png";
 
-import simone1 from './images/simone1.png';
-import simone2 from './images/simone2.png';
-import simone3 from './images/simone3.png';
-import simone4 from './images/simone4.png';
+import simone1 from "./images/simone1.png";
+import simone2 from "./images/simone2.png";
+import simone3 from "./images/simone3.png";
+import simone4 from "./images/simone4.png";
 
-import hmconsciousstart from './images/hmconscious.png';
+import hmconsciousstart from "./images/hmconscious.png";
 
-import hmhome1 from './images/hmhome1.png';
-import hmhome2 from './images/hmhome2.png';
-import hmhome3 from './images/hmhome3.png';
-import hmhome4 from './images/hmhome4.png';
+import hmhome1 from "./images/hmhome1.png";
+import hmhome2 from "./images/hmhome2.png";
+import hmhome3 from "./images/hmhome3.png";
+import hmhome4 from "./images/hmhome4.png";
 
-import pomodorostart from './images/pomodoropinkstart.png';
-import pomodoro1 from './images/pomodoropink.png';
-import pomodoro2 from './images/pomodoroblue.png';
-import pomodoro3 from './images/pomodoromobilepink.png';
-import pomodoro4 from './images/pomodoromobileblue.png';
+import pomodorostart from "./images/pomodoropinkstart.png";
+import pomodoro1 from "./images/pomodoropink.png";
+import pomodoro2 from "./images/pomodoroblue.png";
+import pomodoro3 from "./images/pomodoromobilepink.png";
+import pomodoro4 from "./images/pomodoromobileblue.png";
 
 const info = [
   {
@@ -91,211 +90,222 @@ const info = [
   },
   {
     classtext: "card-container",
-    name: 'Rota App',
+    name: "Rota App",
     imgSRC: rota,
     img1: rotastart,
     img2: rotagoal,
     img3: rotastatus,
     img4: rotaform,
-    pinfo: 'A "Habit tracker" application where you can measure your habits and set your own goals. Created with vanilla Javasctript.',
-    linktext: 'See the code',
-    linkclasstext: 'linktext-show', 
-    link: 'https://github.com/elinordin/habit-tracker',
+    pinfo:
+      'A "Habit tracker" application where you can measure your habits and set your own goals. Created with vanilla Javasctript.',
+    linktext: "See the code",
+    linkclasstext: "linktext-show",
+    link: "https://github.com/elinordin/habit-tracker",
   },
   {
     classtext: "card-container",
-    name: 'Wellbeing Insurance',
+    name: "Wellbeing Insurance",
     imgSRC: feelgoodinsurancestart,
     img1: feelgoodinsurancestart,
     img2: feelgoodinsurancesecond,
     img3: feelgoodinsurancethird,
     img4: feelgoodinsurancelast,
-    pinfo: 'Design project for a own fictional company. Created with Illustrator.',
-    linktext: '',
-    linkclasstext: 'linktext-hidden',
-    link: '',
+    pinfo:
+      "Design project for a own fictional company. Created with Illustrator.",
+    linktext: "",
+    linkclasstext: "linktext-hidden",
+    link: "",
   },
   {
     classtext: "card-container",
-    name: 'Braive',
+    name: "Braive",
     imgSRC: braivestart,
     img1: braivestarts,
     img2: braivetools,
     img3: braivestartmobile,
     img4: braiverecommendmobile,
-    pinfo: 'Project with the company Braive. The focus was on improving their design and usability for their logged in customers. Created with Figma.',
-    linktext: '',
-    linkclasstext: 'linktext-hidden', 
-    link: '',
+    pinfo:
+      "Project with the company Braive. The focus was on improving their design and usability for their logged in customers. Created with Figma.",
+    linktext: "",
+    linkclasstext: "linktext-hidden",
+    link: "",
   },
   {
     classtext: "card-container",
-    name: 'VR-game',
+    name: "VR-game",
     imgSRC: mygamefabric,
     img1: mygamefabricstart,
     img2: mygamefabricinside,
     img3: mygamefabrickeypad,
     img4: mygamefabricsky,
-    pinfo: 'Created a VR game with A-frame and JavaScript.',
-    linktext: 'Play my game',
-    linkclasstext: 'linktext-show', 
-    link: 'https://escapefromhyperisland.github.io/world-8/malin/',
+    pinfo: "Created a VR game with A-frame and JavaScript.",
+    linktext: "Play my game",
+    linkclasstext: "linktext-show",
+    link: "https://escapefromhyperisland.github.io/world-8/malin/",
   },
   {
     classtext: "card-container",
-    name: 'Ricola',
+    name: "Ricola",
     imgSRC: ricolastart,
     img1: ricolastart,
     img2: ricolasecond,
     img3: ricolathird,
     img4: ricolalast,
-    pinfo: 'Project with the company Ricola. The main task was to improve their transparency for their work with sustainability. Created with vanilla JavaScript.',
-    linktext: 'See the Website',
-    linkclasstext: 'linktext-show', 
-    link: 'https://pablogalisteo.github.io/ricola/',
+    pinfo:
+      "Project with the company Ricola. The main task was to improve their transparency for their work with sustainability. Created with vanilla JavaScript.",
+    linktext: "See the Website",
+    linkclasstext: "linktext-show",
+    link: "https://pablogalisteo.github.io/ricola/",
   },
   {
     classtext: "card-container",
-    name: 'Tarot-API',
+    name: "Tarot-API",
     imgSRC: api,
     img1: api,
     img2: api,
     img3: api,
     img4: api,
-    pinfo: 'Created my own web-API. Created it with MongoDB, Robo 3T and Heroku. The plan is to use it for a Tarot-Card application.',
-    linktext: 'See the API',
-    linkclasstext: 'linktext-show', 
-    link: 'https://rws-cards-api.herokuapp.com/api/v1/cards',
+    pinfo:
+      "Created my own web-API. Created it with MongoDB, Robo 3T and Heroku. The plan is to use it for a Tarot-Card application.",
+    linktext: "See the API",
+    linkclasstext: "linktext-show",
+    link: "https://rws-cards-api.herokuapp.com/api/v1/cards",
   },
   {
     classtext: "card-container",
-    name: 'First Aid Therapy',
+    name: "First Aid Therapy",
     imgSRC: firstaidtherapy,
     img1: firstaidtherapystart,
     img2: firstaidtherapysecond,
     img3: firstaidtherapythird,
     img4: firstaidtherapylastlast,
-    pinfo: 'Is an application that boosts you. Created with Vanilla Javascript.',
-    linktext: 'See the Website',
-    linkclasstext: 'linktext-show', 
-    link: 'https://mieown.github.io/MagicEightBall/',
+    pinfo:
+      "Is an application that boosts you. Created with Vanilla Javascript.",
+    linktext: "See the Website",
+    linkclasstext: "linktext-show",
+    link: "https://mieown.github.io/MagicEightBall/",
   },
   {
     classtext: "card-container",
-    name: 'Tounge Twister',
+    name: "Tounge Twister",
     imgSRC: toungetwister,
     img1: toungetwisterstart,
     img2: toungetwisecond,
     img3: toungetwister,
     img4: toungetwistlast,
-    pinfo: 'Is a game where you have to keep your tongue right, you need to pronounce difficult sentences correctly. Created with two web API:s and JavaScript.',
-    linktext: 'Play the game',
-    linkclasstext: 'linktext-show', 
-    link: 'https://mieown.github.io/Tongue-twister/',
+    pinfo:
+      "Is a game where you have to keep your tongue right, you need to pronounce difficult sentences correctly. Created with two web API:s and JavaScript.",
+    linktext: "Play the game",
+    linkclasstext: "linktext-show",
+    link: "https://mieown.github.io/Tongue-twister/",
   },
   {
     classtext: "card-container",
-    name: 'Pomodoro Timer',
+    name: "Pomodoro Timer",
     imgSRC: pomodorostart,
     img1: pomodoro1,
     img2: pomodoro2,
     img3: pomodoro3,
     img4: pomodoro4,
-    pinfo: 'Pomodoro Timer created in React.',
-    linktext: 'Test the Timer',
-    linkclasstext: 'linktext-show', 
-    link: 'https://mieown.github.io/pomodorotimer/',
+    pinfo: "Pomodoro Timer created in React.",
+    linktext: "Test the Timer",
+    linkclasstext: "linktext-show",
+    link: "https://mieown.github.io/pomodorotimer/",
   },
   {
     classtext: "card-container",
-    name: 'HM Studio',
+    name: "HM Studio",
     imgSRC: studioone,
     img1: studiofirst,
     img2: studiosecond,
     img3: studiothird,
     img4: studiofirst,
-    pinfo: 'Project for HM studio 2020 where I worked with the graphics and the packaging. Created with Illustrator, Indesign and photoshop.',
-    linktext: '',
-    linkclasstext: 'linktext-hidden', 
-    link: '',
+    pinfo:
+      "Project for HM studio 2020 where I worked with the graphics and the packaging. Created with Illustrator, Indesign and photoshop.",
+    linktext: "",
+    linkclasstext: "linktext-hidden",
+    link: "",
   },
   {
     classtext: "card-container",
-    name: 'Whoolichen x HM',
+    name: "Whoolichen x HM",
     imgSRC: whoolichen,
     img1: whoolichenfirst,
     img2: whoolichensecond,
     img3: whoolichenthird,
     img4: whoolichenlast,
-    pinfo: 'The project Whoolichen x HM where I worked with the graphics and the packaging. Created with Illustrator, Indesign and photoshop.',
-    linktext: '',
-    linkclasstext: 'linktext-hidden', 
-    link: '',
+    pinfo:
+      "The project Whoolichen x HM where I worked with the graphics and the packaging. Created with Illustrator, Indesign and photoshop.",
+    linktext: "",
+    linkclasstext: "linktext-hidden",
+    link: "",
   },
   {
     classtext: "card-container",
-    name: 'COS',
+    name: "COS",
     imgSRC: coscam,
     img1: coscamstart,
     img2: coscamstart,
     img3: coscamstart,
     img4: coscamstart,
-    pinfo: 'Packaging for a comb. Created with Indesign.',
-    linktext: '',
-    linkclasstext: 'linktext-hidden', 
-    link: '',
+    pinfo: "Packaging for a comb. Created with Indesign.",
+    linktext: "",
+    linkclasstext: "linktext-hidden",
+    link: "",
   },
   {
     classtext: "card-container",
-    name: '& Other Stories',
+    name: "& Other Stories",
     imgSRC: storiesboxtre,
     img1: otherstoriesbox1,
     img2: otherstoriesbox2,
     img3: shoelacepink,
     img4: shoelacepurple,
-    pinfo: 'Packaging for Stories. Created with Indesign.',
-    linktext: '',
-    linkclasstext: 'linktext-hidden', 
-    link: '',
+    pinfo: "Packaging for Stories. Created with Indesign.",
+    linktext: "",
+    linkclasstext: "linktext-hidden",
+    link: "",
   },
   {
     classtext: "card-container",
-    name: 'Simone Rocha x HM',
+    name: "Simone Rocha x HM",
     imgSRC: simonerocha,
     img1: simone1,
     img2: simone2,
     img3: simone3,
     img4: simone4,
-    pinfo: 'Worked in HM:s guest designer project 2020, Simona Rocha. Created the graphics and the packaging. Created with Illustrator and Indesign.',
-    linktext: '',
-    linkclasstext: 'linktext-hidden', 
-    link: '',
+    pinfo:
+      "Worked in HM:s guest designer project 2020, Simona Rocha. Created the graphics and the packaging. Created with Illustrator and Indesign.",
+    linktext: "",
+    linkclasstext: "linktext-hidden",
+    link: "",
   },
   {
     classtext: "card-container",
-    name: 'HM Conscious',
+    name: "HM Conscious",
     imgSRC: hmconscious,
     img1: hmconsciousstart,
     img2: hmconsciousstart,
     img3: hmconsciousstart,
     img4: hmconsciousstart,
-    pinfo: 'Worked with HM Conscious. Created with Illustrator and Indesign.',
-    linktext: '',
-    linkclasstext: 'linktext-hidden', 
-    link: '',
+    pinfo: "Worked with HM Conscious. Created with Illustrator and Indesign.",
+    linktext: "",
+    linkclasstext: "linktext-hidden",
+    link: "",
   },
   {
     classtext: "card-container",
-    name: 'HM Home',
+    name: "HM Home",
     imgSRC: hmhome,
     img1: hmhome1,
     img2: hmhome2,
     img3: hmhome3,
     img4: hmhome4,
-    pinfo: 'Worked with HM Home. Created with Illustrator, Indesign and photoshop.',
-    linktext: '',
-    linkclasstext: 'linktext-hidden', 
-    link: '',
+    pinfo:
+      "Worked with HM Home. Created with Illustrator, Indesign and photoshop.",
+    linktext: "",
+    linkclasstext: "linktext-hidden",
+    link: "",
   },
 ];
 
